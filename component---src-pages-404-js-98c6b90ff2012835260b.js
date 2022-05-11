fb8906d05265e80251e25445569b34d7dc0561cb
Naoto Nishimura <nishimura@nawo.to>
2022-05-11 15:08:29 +0900

    Updates

---
+++ component---src-pages-404-js-98c6b90ff2012835260b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknawo_to=self.webpackChunknawo_to||[]).push([[883],{429:function(t,e,a){a.r(e);var n=a(7294),o=a(7133),l=a(4501);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return n.createElement(o.Z,{location:a,title:u},n.createElement(l.Z,{title:"404: Not Found | "+u}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-98c6b90ff2012835260b.js.map
